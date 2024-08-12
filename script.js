1d64c77d6724ea89550890bd9c3fa6a1b5306353
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-08-12 16:05:34 +0700

    Added a feature to choose white or rainbow pen, fixed a conditional check on custom grid size, renamed the selectors

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".container.right");
 const gridSizeButton = document.querySelector(".btn-grid-size");
+const whitePenButton = document.querySelector(".btn-white-pen");
+const rainbowPenButton = document.querySelector(".btn-rainbow-pen");
 
 const createGrid = (gridSize) => {
   const totalGrids = gridSize * gridSize;
@@ -12,21 +14,50 @@ const createGrid = (gridSize) => {
     grid.style.flex = `0 0 ${gridPercentage}%`;
 
     container.appendChild(grid);
-
-    grid.addEventListener("mouseover", () => {
-      grid.style.backgroundColor = "#000";
-    });
   }
 };
 
-createGrid(16);
+const createCustomGridSize = () => {
+  const userGridInput = prompt("Enter new grid size (1-100).");
 
-gridSizeButton.addEventListener("click", () => {
-  container.innerHTML = "";
+  if (userGridInput === null) return;
 
-  const userGridInput = parseInt(prompt("Enter new grid size (1-100)."));
+  if (isNaN(userGridInput) || userGridInput === "") {
+    alert("Please enter a valid input.");
+    return;
+  }
 
-  if (userGridInput > 100) return alert("Maximum grid size is 100.");
+  const gridSize = parseInt(userGridInput);
 
+  if (gridSize < 1 || gridSize > 100) {
+    alert("Grid size must be between 1-100.");
+    return;
+  }
+
+  removePreviousGrid();
   createGrid(userGridInput);
-});
+};
+
+const getPenMode = (selectedPen) => {
+  container.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("grid")) {
+      e.target.style.backgroundColor = selectedPen();
+    }
+  });
+};
+
+const removePreviousGrid = () => (container.innerHTML = "");
+const getWhiteColor = () => "#fff";
+const getRandomColor = () => {
+  const redColorValue = Math.floor(Math.random() * 256);
+  const greenColorValue = Math.floor(Math.random() * 256);
+  const blueColorValue = Math.floor(Math.random() * 256);
+
+  return `rgb(${redColorValue}, ${greenColorValue}, ${blueColorValue})`;
+};
+
+gridSizeButton.addEventListener("click", () => createCustomGridSize());
+whitePenButton.addEventListener("click", () => getPenMode(getWhiteColor));
+rainbowPenButton.addEventListener("click", () => getPenMode(getRandomColor));
+
+createGrid(64);
